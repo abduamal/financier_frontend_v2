b86156e19d6c647fe14c63b59b839f54fd9d2cb1
Amal Abdu <abduamal@ymail.com>
2021-10-04 16:32:26 -0500

    set up authentication routes + routes for home and dashboard

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NavigationBar from "./components/Navbar";
+import Signup from "./components/auth/Signup";
+import Login from "./components/auth/Login";
+import Homepage from "./components/Homepage";
+import Dashboard from "./components/Dashboard";
+import withAuth from "./components/auth/withAuth"
 
 function App() {
-  return <div className="App">App component</div>
+  return(
+    <div>
+      <Router>
+        <NavigationBar />
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/dashboard" component={withAuth(Dashboard)} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={Signup} />
+        </Switch>
+      </Router>
+    </div>
+  )
 }
 
 export default App
