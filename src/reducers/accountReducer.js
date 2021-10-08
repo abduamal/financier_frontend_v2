dc491873b14b7d8029e758b69fa47e039df1bb53
Amal Abdu <abduamal@ymail.com>
2021-10-08 16:48:10 -0500

    add accountReducer to the redux store

---
+++ src/reducers/accountReducer.js
@@ -0,0 +1,20 @@
+export default function accountReducer(state = {accounts: []}, action ) {
+  console.log(action)
+  switch (action.type) {
+    case 'FETCH_ACCOUNTS':
+      return {accounts: action.payload}
+    // case 'ADD_SHOP':
+    //   return {...state, accounts: [...state.accounts, action.payload]}
+    // case 'ADD_PRODUCT':
+    //   let accounts = state.accounts.map(account => {
+    //     if (account.id === action.payload.id) {
+    //       return action.payload
+    //     } else {
+    //       return account
+    //     }
+    //   })
+    //   return {...state, accounts: accounts}
+    default:
+      return state
+  }
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
 import authReducer from './auth'
+import accountReducer from './accountReducer'
 
 export default combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  accounts: accountReducer
 })
