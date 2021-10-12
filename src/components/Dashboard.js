946c38814c156bf4661fa1909e33a47cb028dc25
Amal Abdu <abduamal@ymail.com>
2021-10-12 13:34:52 -0500

    add a button for a list of accounts on the dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,16 +1,15 @@
 import React from "react";
 import { connect } from "react-redux";
-import Alert from 'react-bootstrap/Alert'
+// import AccountsContainer from '../containers/AccountsContainer.js'
+import Button from 'react-bootstrap/Button'
+// import Alert from 'react-bootstrap/Alert'
 
 class Dashboard extends React.Component {
   render() {
     return (
-      <Alert variant="success">
-        <Alert.Heading>Hey, user. Welcome to your dashboard</Alert.Heading>
-        <p>
-          This is a protected route. It should only be visible to a user who is signed in.
-        </p>
-      </Alert>
+      <React.Fragment>
+        <Button href="/accounts" variant="primary">Accounts</Button>
+      </React.Fragment>
     );
   }
 }
